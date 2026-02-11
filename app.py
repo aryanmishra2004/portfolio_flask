df941143781abc0607e168f4c12a9757f67296e1
aryanmishra2004 <aryanmishra93018@gmail.com>
2026-02-11 09:24:29 +0530

    Add MongoDB connection

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
 import os
 
 app = Flask(__name__)
+
+# MongoDB connection
+client = MongoClient("mongodb+srv://aryannmishra65_db_user:T6AkVXVMvcptrbhl@cluster0.67bu29o.mongodb.net/?appName=Cluster0")
+db = client["aryandb"]
 
 @app.route("/")
 def index():
